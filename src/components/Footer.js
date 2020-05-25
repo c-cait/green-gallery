48ea6f3f05c53a7b6c2c9eb347a8c0a48bbaee2b
c-cait <caitlinciaramella@gmail.com>
2020-05-25 03:02:47 -0500

    completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import './Footer.css';
+import {RiCopyrightLine} from 'react-icons/ri'
 
 //use semantic tags
 function Footer(){
     return (
-        <footer>
-            Footer.js
+        <footer className='footer-container'>
+           <div className='copyright-footer'> 
+          <span className='copy-icon'><RiCopyrightLine /> </span> 
+           2020 Caitlin Ciaramella </div>
         </footer>
     )
 }
